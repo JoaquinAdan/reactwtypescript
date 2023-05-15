77763f54315e2bb4c1334734a1237924cd6a125f
JoaquinAdan <rodriguezjdev@gmail.com>
2023-05-15 14:15:55 -0300

    global types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
-import { useState } from 'react';
-import type { MouseEventHandler } from 'react';
-import { RandomFox } from '@/components/RandomFox';
+import { useState, MouseEventHandler } from 'react';
+import { LazyImage } from '@/components/RandomFox';
 
 // generate random random function that returns a number between 1 and 123 (inclusive)
 const randomNum = () => Math.floor(Math.random() * 123) + 1;
@@ -9,26 +8,34 @@ const randomNum = () => Math.floor(Math.random() * 123) + 1;
 // generate simple unique id
 const generateId = () => Math.random().toString(36).substring(2, 15);
 
-type ImageItems = { id: string; url: string };
-
 export default function Home(): JSX.Element {
-  const [images, setImages] = useState<Array<ImageItems>>([]);
+  const [images, setImages] = useState<Array<IFoxImageItems>>([]);
 
   const addNewFox: MouseEventHandler<HTMLButtonElement> = (event) => {
-    const newImageItem: ImageItems = {
+    const newImageItem: IFoxImageItems = {
       id: generateId(),
       url: `https://randomfox.ca/images/${randomNum()}.jpg`,
     };
-    setImages([newImageItem, ...images]);
+    setImages([...images, newImageItem]);
   };
 
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello Vitu!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <button onClick={addNewFox}>Add new fox</button>
-      {images.map(({ id, url }) => (
+      {images.map(({ id, url }, index) => (
         <div key={id} className="p-4">
-          <RandomFox image={url} />
+          <LazyImage
+            src={url}
+            width={320}
+            height="auto"
+            title="Random fox"
+            className="rounded bg-gray-300"
+            onLazyLoad={(img) =>
+              console.log(`Image #${index + 1} cargada. Nodo:`, img)
+            }
+            onClick={() => console.log('hola')}
+          />
         </div>
       ))}
     </main>
--- src/components/RandomFox.tsx
+++ src/components/RandomFox.tsx
@@ -1,10 +1,45 @@
-import { useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
+import type { ImgHTMLAttributes } from 'react';
 
-type Props = { image: string };
+type LazyImageProps = {
+  src: string;
+  onLazyLoad?: (img: HTMLImageElement) => void;
+};
 
-export const RandomFox = ({ image }: Props): JSX.Element => {
+type ImageNative = ImgHTMLAttributes<HTMLImageElement>;
+
+type Props = LazyImageProps & ImageNative;
+
+const transparentBackground =
+  'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=';
+
+export const LazyImage = ({
+  src,
+  onLazyLoad,
+  ...imgProps
+}: Props): JSX.Element => {
   const node = useRef<HTMLImageElement>(null);
-  return (
-    <img ref={node} width={320} height="auto" src={image} className="rounded" />
-  );
+  const [isLazyLoaded, setIsLazyLoaded] = useState(false);
+  const [currentSrc, setCurrentSrc] = useState(transparentBackground);
+
+  useEffect(() => {
+    if (isLazyLoaded) return;
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setCurrentSrc(src);
+          observer.disconnect();
+          setIsLazyLoaded(true);
+          if (typeof onLazyLoad === 'function')
+            if (node.current) onLazyLoad(node.current);
+        }
+      });
+    });
+    if (node.current) observer.observe(node.current);
+    return () => observer.disconnect();
+  }, [src, onLazyLoad, isLazyLoaded]);
+
+  useEffect(() => {}, [currentSrc]);
+
+  return <img src={currentSrc} ref={node} {...imgProps} />;
 };
